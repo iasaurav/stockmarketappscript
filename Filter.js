500013d4c04fe9c4428b4831893da0a813485e0b
iasaurav <101176678+iasaurav@users.noreply.github.com>
2024-07-19 12:54:51 +0530

    Filter.js

--- Filter.js
+++ Filter.js
@@ -1,5 +1,16 @@
+ZERODHA.pasteFormulaniftt();
+ZERODHA.pasteFormulabniftt();
+
+
 function pasteFormula() {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('apk');
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('sheet2');
   var formula = '=YOUR_FORMULA_HERE';
   sheet.getRange('A1').setFormula(formula);
+}
+
+
+function pasteFormulabniftt() {
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('sheet2');
+  var formula = '=FILTER(A1:F,C1:C="BANKNIFTY",E1:E>AE2-300,E1:E<AE2+600,D1:D=AD2)';
+  sheet.getRange('AN1').setFormula(formula);
 }
