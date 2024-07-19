e3835cbc93499fa0d4b3ff9aa70e7e1bd66a98e8
iasaurav <101176678+iasaurav@users.noreply.github.com>
2024-07-19 13:02:01 +0530

    Filter.js

--- Filter.js
+++ Filter.js
@@ -1,3 +1,6 @@
+
+
+
 ZERODHA.pasteFormulaniftt();
 ZERODHA.pasteFormulabniftt();
 
@@ -13,4 +16,20 @@ function pasteFormulabniftt() {
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('sheet2');
   var formula = '=FILTER(A1:F,C1:C="BANKNIFTY",E1:E>AE2-300,E1:E<AE2+600,D1:D=AD2)';
   sheet.getRange('AN1').setFormula(formula);
+}
+
+
+
+function pasteFormulafniftt() {
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('sheet2');
+  var formula = '=FILTER(A1:F,C1:C="FINNIFTY",E1:E>AE3-100,E1:E<AE3+300,D1:D=AD3)';
+  sheet.getRange('AU1').setFormula(formula);
+}
+
+
+
+function pasteFormulacriftt() {
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('sheet2');
+  var formula = '=FILTER(A1:F,C1:C="CRUDEOIL",E1:E>AE4-100,E1:E<AE4+300,D1:D=AD4)';
+  sheet.getRange('BC1').setFormula(formula);
 }
