f9682d7b344509de7b3c9460a164fc09d4f5ace3
iasaurav <101176678+iasaurav@users.noreply.github.com>
2024-07-20 22:11:07 +0530

    Filter.js

--- Filter.js
+++ Filter.js
@@ -65,10 +65,9 @@ function pasteFormulacriftt() {
 
 
 <a href='https://tv.upstox.com/charts/NSE_FO%7C65335'target='_blank'>NIFTY 25JUL24 24350 CE</a>
-function upstox(h,i,j,k) {
-const options = { year: 'numeric', month: 'short', day: '2-digit' };
-const formattedDate = j.toLocaleDateString('en-GB', options).replace(/ /g, ' ');
+function upstox(h,i,j,k,l) {
+let formattedDate = j.toLocaleDateString('en-GB', { day: '2-digit', month: 'short', year: '2-digit' }).replace(/ /g, '').toUpperCase();
   let tradingview='https://tv.upstox.com/charts/NSE_FO|';
-let a="<a href='"+tradingvie+h+"?isPopup=true'target='_blank'>"+i+formattedDate+k+"</a>"
+let a="<a href='"+tradingvie+h+"?isPopup=true'target='_blank'>"+i+" "+formattedDate+" "+k+l"</a>"
   return a;
   }
