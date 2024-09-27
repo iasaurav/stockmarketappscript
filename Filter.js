6aea958f890b21d9699abf911c8943f772376cc6
iasaurav <101176678+iasaurav@users.noreply.github.com>
2024-09-27 16:01:33 +0530

    Update Filter.js

--- Filter.js
+++ Filter.js
@@ -1,57 +1,9 @@
-function fillFormulaInColumnupstox() {
-  // Get the active spreadsheet
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('sheet3');
-
-  // Define the formula you want to apply
-  var formula = '=if(A1="","",upstox(A1,C1,D1,E1,F1,C1))';  // Replace with your actual formula
-
-  // Fill the range G1:G60 with the formula
-  var range = sheet.getRange('G1:G');
-  range.setFormula(formula);
-}
-
-
-function fillFormulaInColumndhan() {
-  // Get the active spreadsheet
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('sheet3');
-
-  // Define the formula you want to apply
-  var formula = '=if(A1="","",DHAN(A1,C1,D1,E1,F1,C1))';  // Replace with your actual formula
-
-  // Fill the range G1:G60 with the formula
-  var range = sheet.getRange('H1:H');
-  range.setFormula(formula);
-}
-
-
-
-
-function pasteFormulaniftt() {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('sheet2');
-  var formula = '=FILTER(A1:F,C1:C="NIFTY",E1:E>AE1-100,E1:E<AE1+300,D1:D=AD1)';
-  sheet.getRange('AG1').setFormula(formula);
-}
-
-
-function pasteFormulabniftt() {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('sheet2');
-  var formula = '=FILTER(A1:F,C1:C="BANKNIFTY",E1:E>AE2-300,E1:E<AE2+600,D1:D=AD2)';
-  sheet.getRange('AN1').setFormula(formula);
-}
-
-
-
-function pasteFormulafniftt() {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('sheet2');
-  var formula = '=FILTER(A1:F,C1:C="FINNIFTY",E1:E>AE3-100,E1:E<AE3+300,D1:D=AD3)';
-  sheet.getRange('AU1').setFormula(formula);
-}
-
-
-
-
-
-
+sheet3g1:G=if(A1="","",upstox(A1,C1,D1,E1,F1,C1))
+sheet3h1:h=if(A1="","",DHAN(A1,C1,D1,E1,F1,C1))
+sheet2AG1=FILTER(A1:F,C1:C="NIFTY",E1:E>AE1-100,E1:E<AE1+300,D1:D=AD1)
+sheet2AN1=FILTER(A1:F,C1:C="BANKNIFTY",E1:E>AE2-300,E1:E<AE2+600,D1:D=AD2)
+sheet2Au1=FILTER(A1:F,C1:C="FINNIFTY",E1:E>AE3-100,E1:E<AE3+300,D1:D=AD3)
+  
 
 function pasteFormulaseniftt() {
   var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('sheet2');
